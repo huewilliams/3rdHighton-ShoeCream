ec0f0125f6204102cdeacbb0447728d318613cba
huewilliams <37013834+huewilliams@users.noreply.github.com>
2018-06-04 10:08:00 +0900

    shop.php

---
+++ shop.php
@@ -0,0 +1,35 @@
+<?php
+$query = $_GET['query'];
+$client_id = "7r3R46FZEiwIohzd6WCo";
+$client_secret = "ylBgKZx61S";
+$encText = urlencode($query);
+$url = "https://openapi.naver.com/v1/search/shop.json?query=".$encText; // json 결과
+//  $url = "https://openapi.naver.com/v1/search/blog.xml?query=".$encText; // xml 결과
+$is_post = false;
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, $is_post);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$headers = array();
+$headers[] = "X-Naver-Client-Id: ".$client_id;
+$headers[] = "X-Naver-Client-Secret: ".$client_secret;
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+$response = curl_exec ($ch);
+$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close ($ch);
+if($status_code == 200) {
+    // echo $response;
+    $arr = json_decode($response, true);
+    $data = $arr["items"];
+    // print_r($data);
+    usort($data, function($a, $b) {
+        return $a['lprice'] <=> $b['lprice'];
+    });
+    // print_r($data);
+    $json = json_encode($data,JSON_UNESCAPED_UNICODE);
+    echo str_replace('\\','',$json);
+} else {
+    echo "Error 내용:".$response;
+}
+?>
